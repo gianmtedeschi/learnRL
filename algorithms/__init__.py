4bc87c023a0e7a024a2d8fd19e74f97f6e2ec636
StefanoFerrara2 <stefano2.ferrara@mail.polimi.it>
2024-04-12 17:47:01 +0200

    Update __init__.py

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,3 +1,5 @@
 # from algorithms.pgpe import *
 from algorithms.policy_gradient import *
 from algorithms.pg_split import *
+from algorithms.pg_split_angles_variation import*
+from algorithms.pg_split_VM import*
